751efb5b9b01c078520bba12eb2031ec08fe51c5
mubin6th <mubin.muhammad.the.7th@gmail.com>
2025-11-04 21:02:51 +0600

    Complete re-structure of the project to allow multiple themes. Currently charcoal-monochrome-dark is supported.

--- base/fish/config.fish
+++ base/fish/config.fish
@@ -5,5 +5,3 @@ function fish_greeting
 end
 
 alias ls="eza -l --git --color=always --icons=never --sort=extension --no-user --no-permissions"
-
-set -U fish_user_paths /home/mubin/bin_apps/zig-x86_64-linux-0.14.1 $fish_user_paths
